204b05c9fc8cc5b54fe335341bb908752766a90b
solenwa <solen.wanono@gmail.com>
2023-04-04 14:03:03 +0200

    Done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,47 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.intervalID = setInterval(() => {
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+      this.currentTime +=1
+    },1000)
+
   }
 
   getMinutes() {
     // ... your code goes here
+    return this.minutes = Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.seconds = Math.floor(this.currentTime%60);
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+      let valueString = value.toString();
+
+      return valueString.padStart(2, `0`)
   }
 
   stop() {
     // ... your code goes here
+      return clearInterval(this.intervalID);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
